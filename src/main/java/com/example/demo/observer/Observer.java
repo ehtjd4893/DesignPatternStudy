45104781324906bc3796e5e268e09be4e86e633d
dosung_park <nsop4893@gmail.com>
2025-02-11 14:05:18 +0900

    Git init

---
+++ src/main/java/com/example/demo/observer/Observer.java
@@ -0,0 +1,4 @@
+package com.example.demo.observer;
+
+public interface Observer {
+}
---
+++ src/main/java/com/example/demo/observer/Subject.java
@@ -0,0 +1,11 @@
+package com.example.demo.observer;
+
+public interface Subject {
+
+  void registerObserver(Observer observer);
+
+  void removeObserver(Observer observer);
+
+  void setState();
+
+}
